52cd8f6dc4256f113b8c0e530e52a247305ce778
jegomez <jegomez@veracruz.gob.mx>
2023-05-08 18:50:05 -0600

    CHANGE prompt

--- src/chatGpt.ts
+++ src/chatGpt.ts
@@ -11,25 +11,33 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 const generatePromp = (text: string) => {
-  return `Return JSON object validate feelings[""], steps[""] from phrase(spanish):${text}`
+  return `Provide a JSON object with the detected emotions and tips for the phrase ${text} in Spanish.`
 }
 
 export const getFeelings = async (req: Request, res: Response) => {
   try {
+    const text: string = req.body.text
+    console.log(text);
+    if (!text) res.status(400).json({ error: 'Por favor, incluya un texto de como se siente' })
+
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePromp(req.body.text),
+      prompt: generatePromp(text),
       temperature: 0.7,
       max_tokens: 256,
     });
-    const text = completion.data.choices[0].text
-    const newText = text?.replace(/\n/g, "")
+
+    const responsetext = completion.data.choices[0].text
+    const newText = responsetext?.replace(/\n/g, "")
+
     if (newText) {
       res.status(200).json(JSON.parse(newText))
     } else {
       res.status(400).json({ error: 'No se pudo obtener una respuesta de tu texto' })
     }
+
   } catch (e) {
+    console.log(e, "ERROR GET INFO");
     res.status(400).send({ error: "Error al generar una respuesta, vuelve a intentar más tarde" })
   }
 };
